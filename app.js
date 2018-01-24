f24db9456d26199a17f4bef5c3da2d4b544470c1
Jason Liu <jasonliu1999@gmail.com>
2018-01-23 23:11:29 -0500

    Added random bullet generation

--- app.js
+++ app.js
@@ -102,6 +102,42 @@ Player.update = function () {
     return pack;
 }
 
+var Bullet = function (angle) {
+    var self = Entity();
+    self.id = Math.random();
+    self.spdX = Math.cos(angle/180*Math.PI) * 10;
+    self.spdY = Math.sin(angle/180*Math.PI) * 10;
+
+    self.timer = 0;
+    self.toRemove = false;
+    var super_update = self.update;
+    self.update = function () {
+        if(self.timer++ > 100)
+            self.toRemove = true;
+        super_update();
+    }
+    Bullet.list[self.id] = self;
+}
+Bullet.list = {};
+
+Bullet.update = function () {
+    if(Math.random() < 0.05){
+        Bullet(Math.random()*360);
+    }
+
+    var pack = [];
+    for(var i in Bullet.list){
+        var bullet = Bullet.list[i];
+        bullet.update();
+        pack.push({
+            x:bullet.x,
+            y:bullet.y,
+        });
+
+    }
+    return pack;
+}
+
 var io = require('socket.io')(serv,{});
 
 io.sockets.on('connection', function (socket) {
@@ -121,7 +157,11 @@ io.sockets.on('connection', function (socket) {
 });
 
 setInterval(function () {
-    var pack = Player.update();
+    var pack = {
+        player: Player.update(),
+        bullet: Bullet.update(),
+    }
+
 
     for(var i in SOCKET_LIST){
         var socket = SOCKET_LIST[i];
