e667cd5e87c5dc04f39e3331cbbd6645ee57d0e0
Jason Liu <jasonliu1999@gmail.com>
2018-01-25 10:36:07 -0500

    Server config update

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use('/assets', express.static(__dirname + '/assets'));
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
  
-server.listen(server_port, server_ip_address, function () {
+serv.listen(server_port, server_ip_address, function () {
   console.log( "Listening on " + server_ip_address + ", port " + server_port )
 });
 
