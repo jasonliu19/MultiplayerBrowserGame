9bfcb3160a822363600a63d3185ff625528be35a
Jason Liu <jasonliu1999@gmail.com>
2018-01-25 11:04:17 -0500

    Edited server ip

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ app.get('/', function (req, res){
 app.use('/client', express.static(__dirname + '/client'));
 app.use('/assets', express.static(__dirname + '/assets'));
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP;
  
 serv.listen(server_port, server_ip_address, function () {
   console.log( "Listening on " + server_ip_address + ", port " + server_port )
